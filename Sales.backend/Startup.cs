5450dbb6e0a5e310dd3f9e75cfbed5d1ccb22c06
kikemadrigal <kikemadrigal@hotmail.com>
2018-09-16 23:38:46 +0200

    Añadidos proyectos de SAles form Xamarin, android, IOS, UWP Xamarin, backend MVC, Web API, Class library Common

---
+++ Sales.backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sales.backend.Startup))]
+namespace Sales.backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
